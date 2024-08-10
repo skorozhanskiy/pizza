f6472c931cac3d4be2ad29ad76c447f7e72439dd
Andrey Nikulochkin <nikulochkin.a@yandex.ru>
2024-08-10 23:24:43 +0300

    #13

--- components/shared/products/index.tsx
+++ components/shared/products/index.tsx
@@ -1,71 +1,43 @@
 import React from 'react';
 import { PizzaCard } from '../../shared/pizza-card';
 import Title from 'antd/es/typography/Title';
+import { SectionContainer } from '../../shared/section-container';
 
 interface Props {
   className?: string;
 }
-
+const pizza = [{ titleName: 'Комбо', id: 'Комбо' }];
 export const Products: React.FC<Props> = ({ className }) => {
   return (
     <div className={className}>
-      <div id="Пиццы" style={{ display: 'flex', flexDirection: 'column', gap: '50px' }}>
-        <Title level={2} style={{ fontWeight: 800, marginBottom: '0px' }}>
-          Пиццы
-        </Title>
-        <div
-          style={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            gap: '50px',
-          }}>
-          <PizzaCard />
-          <PizzaCard />
-          <PizzaCard />
-          <PizzaCard />
-          <PizzaCard />
-        </div>
-      </div>
-      <div
-        className=""
-        id="Комбо"
-        style={{ display: 'flex', flexDirection: 'column', gap: '50px' }}>
-        <Title level={2} style={{ fontWeight: 800, marginBottom: '0px' }}>
-          Пиццы
-        </Title>
-        <div
-          style={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            gap: '50px',
-          }}>
-          <PizzaCard />
-          <PizzaCard />
-          <PizzaCard />
-          <PizzaCard />
-          <PizzaCard />
-        </div>
-      </div>
-      <div
-        className=""
-        id="Закуски"
-        style={{ display: 'flex', flexDirection: 'column', gap: '50px' }}>
-        <Title level={2} style={{ fontWeight: 800, marginBottom: '0px' }}>
-          Пиццы
-        </Title>
-        <div
-          style={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            gap: '50px',
-          }}>
-          <PizzaCard />
-          <PizzaCard />
-          <PizzaCard />
-          <PizzaCard />
-          <PizzaCard />
-        </div>
-      </div>
+      <SectionContainer titleName="Пиццы" categoryId={1}>
+        <PizzaCard />
+        <PizzaCard />
+      </SectionContainer>
+      <SectionContainer titleName=" Комбо" categoryId={2}>
+        <PizzaCard />
+        <PizzaCard />
+      </SectionContainer>
+      <SectionContainer titleName=" Закуски" categoryId={3}>
+        <PizzaCard />
+        <PizzaCard />
+      </SectionContainer>
+      <SectionContainer titleName=" Коктейли" categoryId={4}>
+        <PizzaCard />
+        <PizzaCard />
+      </SectionContainer>
+      <SectionContainer titleName=" Кофе" categoryId={5}>
+        <PizzaCard />
+        <PizzaCard />
+      </SectionContainer>
+      <SectionContainer titleName=" Напитки" categoryId={6}>
+        <PizzaCard />
+        <PizzaCard />
+      </SectionContainer>
+      <SectionContainer titleName=" Десерты" categoryId={7}>
+        <PizzaCard />
+        <PizzaCard />
+      </SectionContainer>
     </div>
   );
 };
--- components/shared/section-container/index.tsx
+++ components/shared/section-container/index.tsx
@@ -1,18 +1,41 @@
+'use client';
 import React from 'react';
 import { Flex } from 'antd';
 import Title from 'antd/es/typography/Title';
-
+import { useIntersection } from 'react-use';
+import { useCategoryStore } from '@/store';
 interface Props {
   className?: string;
-  name?: string;
+  titleName?: string;
   children?: React.ReactNode;
+  categoryId: number;
 }
 
-export const SectionContainer: React.FC<Props> = ({ className, name, children }) => {
+export const SectionContainer: React.FC<Props> = ({
+  className,
+  titleName,
+  children,
+  categoryId,
+}) => {
+  const setActiveCategoryId = useCategoryStore((state) => state.setActiveId);
+  const intersectionRef = React.useRef(null);
+  const intersection = useIntersection(intersectionRef, {
+    threshold: 0.4,
+  });
+  React.useEffect(() => {
+    if (intersection?.isIntersecting) {
+      setActiveCategoryId(categoryId);
+      console.log(categoryId, titleName);
+    }
+  }, [categoryId, intersection?.isIntersecting, titleName]);
   return (
-    <Flex className={className} style={{ flexDirection: 'column', gap: '20px' }}>
-      <Title level={1} style={{ fontWeight: 800, marginBottom: '0px' }}>
-        {name}
+    <Flex
+      id={titleName}
+      ref={intersectionRef}
+      className={className}
+      style={{ flexDirection: 'column', gap: '20px' }}>
+      <Title level={2} style={{ fontWeight: 800, marginBottom: '0px' }}>
+        {titleName}
       </Title>
       <Flex style={{ justifyContent: 'space-around', flexWrap: 'wrap', gap: '50px' }}>
         {children}
