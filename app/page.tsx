1687eec40c579a6380ce512b57fea54e16cb9152
Andrey Nikulochkin <nikulochkin.a@yandex.ru>
2024-08-10 19:38:06 +0300

    #12

--- app/page.tsx
+++ app/page.tsx
@@ -9,20 +9,16 @@ export default function Home() {
         display: 'grid',
         gridTemplateColumns: '250px 1fr',
         gridTemplateRows: 'auto',
-        gap: '40px',
+        rowGap: '20px',
+        columnGap: '40px',
       }}>
       <div
         className=""
         style={{
           gridColumn: '1/3',
           gridRow: '1',
-          position: 'sticky',
-          top: 0,
           paddingTop: '20px',
-          paddingBottom: '30px',
           backgroundColor: 'white',
-          boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.1)',
-          zIndex: 1,
         }}>
         <Title
           level={2}
@@ -30,16 +26,30 @@ export default function Home() {
             fontWeight: 800,
             fontSize: 36,
             lineHeight: '49px',
-            marginBottom: '15px',
+            marginBottom: '0px',
           }}>
           Все пиццы
         </Title>
+      </div>
+      <div
+        className=""
+        style={{
+          gridColumn: '1/3',
+          gridRow: '2',
+          position: 'sticky',
+          top: 0,
+          paddingTop: '20px',
+          paddingBottom: '20px',
+          backgroundColor: 'white',
+          // boxShadow: '-1px 76px 28px -8px rgba(34, 60, 80, 0.07)',
+          zIndex: 1,
+        }}>
         <Categories />
       </div>
-      <div className="" style={{ gridColumn: '1', gridRow: '2' }}>
+      <div className="" style={{ gridColumn: '1', gridRow: '3' }}>
         <Filters />
       </div>
-      <div className="" style={{ gridColumn: '2', gridRow: '2' }}>
+      <div className="" style={{ gridColumn: '2', gridRow: '3' }}>
         <Products />
       </div>
     </div>
