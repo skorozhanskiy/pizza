08798ad2add4bbd8745fee5f1c86ca2372a2803d
Andrey Nikulochkin <nikulochkin.a@yandex.ru>
2024-08-16 14:00:41 +0300

    #12

--- components/shared/search-products/index.tsx
+++ components/shared/search-products/index.tsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import { GeneralSearch } from '@/components/ui';
 import styles from './search-products.module.scss';
+import { Flex } from 'antd';
+import Link from 'next/link';
 interface Props {
   className?: string;
 }
@@ -23,7 +25,6 @@ export const SearchProducts: React.FC<Props> = ({ className }) => {
         }}></div>
       <div className={className}>
         <GeneralSearch
-          className={styles.container}
           onChange={() => {}}
           onFocus={() => setFocused(true)}
           onBlur={() => setFocused(false)}
@@ -35,6 +36,20 @@ export const SearchProducts: React.FC<Props> = ({ className }) => {
             zIndex: 150,
           }}
         />
+        <div
+          className={`${focused && styles.focused} ${styles.container} ${
+            focused ? true : 'hidden'
+          }`}>
+          <Link href="#" className={styles.link}>
+            <img
+              src="https://media.dodostatic.net/image/r:292x292/11EE7D612FC7B7FCA5BE822752BEE1E5.avif"
+              width={32}
+              height={32}
+              alt="изображение продукта"
+            />
+            <span>5:38</span>
+          </Link>
+        </div>
       </div>
     </>
   );
